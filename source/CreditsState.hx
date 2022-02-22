9485724a32ea73cb76941a4feb2767681f1d11df
Rovwhjw <96930237+Rovwhjw@users.noreply.github.com>
2022-02-22 06:31:01 +0300

    Add files via upload

--- source/CreditsState.hx
+++ source/CreditsState.hx
@@ -46,6 +46,8 @@ class CreditsState extends MusicBeatState
 		add(grpOptions);
 
 		var pisspoop = [ //Name - Icon name - Description - Link - BG Color
+            ['Modpack Android'],
+            ['TriendHAXK',		    'bf-neo',		    'Make a modpacks The Port',	 'https://youtube.com/channel/UCudRtwHVvuaQraeRoT6zKJg',	'0xFFE01F32'],
 			['Psych Engine Android Port'],
 			['M.A. Jigsaw',		    'majigsaw',		    'Main Coder of The Port',	 'https://www.youtube.com/channel/UC2Sk7vtPzOvbVzdVTWrribQ',	'0xFFC30085'],
 			['Psych Engine Team'],
